dce8a340faaa062ab401cc573c35943e8c1a65d1
SHYAM SUNDAR ASHOK <Shyam.sundar.Ashok@aa.com>
2017-12-30 22:09:53 -0600

    Contional Rendering of Post Detail if no data
    Contional Rendering of Post Detail if no data

--- frontend/src/components/post-detail.js
+++ frontend/src/components/post-detail.js
@@ -12,29 +12,40 @@ import { formatDate } from '../utils/helper';
 const PostDetail = (props) => {
   const { id, title, body, author, timestamp, voteScore, comments = [],
     onUpvotePost, onDownvotePost } = props;
-  
-  return (
-    <div>
-      <div className='content-container-post'>
-        
-        <div className='post-detail'>
-          <h2 className='heading'>{title}</h2>
-          <div className='post-info'>
-            <p className='author-date-time'>by <b>{author}</b> at {formatDate(timestamp)}</p>
-            <Stats
-              onUpvote={() => onUpvotePost(id)}
-              onDownvote={() => onDownvotePost(id)}
-              voteScore={voteScore}
-            />
-          </div>  
-          <p className='post-body'>{body}</p>
-          <ContentControl type={CONTENT_POSTS} id={id} commentsCount={comments.length} />
+    if (id === undefined)
+    {
+      return (
+        <div>
+          <h3>Page not found</h3>
+          <p>The post you are looking for does not exist.</p>
         </div>
-      </div>
-      <LineSeparator />
-      <CommentList postId={id} comments={comments} />
-    </div>
-  )
+      )  
+    }
+    else
+    {
+      return (
+        <div>
+          <div className='content-container-post'>        
+            <div className='post-detail'>
+              <h2 className='heading'>{title}</h2>
+              <div className='post-info'>
+                <p className='author-date-time'>by <b>{author}</b> at {formatDate(timestamp)}</p>
+                <Stats
+                  onUpvote={() => onUpvotePost(id)}
+                  onDownvote={() => onDownvotePost(id)}
+                  voteScore={voteScore}
+                />
+              </div>  
+              <p className='post-body'>{body}</p>
+              <ContentControl type={CONTENT_POSTS} id={id} commentsCount={comments.length} />
+            </div>
+          </div>
+          <LineSeparator />
+          <CommentList postId={id} comments={comments} />
+        </div>
+      )
+    }
+  
 }
 
 export default connect(
